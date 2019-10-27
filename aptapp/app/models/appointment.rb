4e674b7f1945795f3fa274e43712ee9d8fda46e2
Divyanshu Shekhar <divyanshu.shekhar01@gmail.com>
2019-10-27 17:18:25 -0500

    +validation on all attributes

--- aptapp/app/models/appointment.rb
+++ aptapp/app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :patient
   belongs_to :physician
+  validates :visit, presence: true
+  validates :physician, presence: true
+  validates :patient, presence: true
 end
--- aptapp/app/models/physician.rb
+++ aptapp/app/models/physician.rb
@@ -1,4 +1,6 @@
 class Physician < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
+  validates :name, presence: true
+  validates :speciality, presence: true
 end
